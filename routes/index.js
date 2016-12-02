49c3e568678a36971e91dfadf972b51232541fa7
Roger <ipadaxe@gmail.com>
2016-12-03 02:12:55 +0800

    Display ETHER coin on the index page. Note: Open the geth before creating server.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var coinbase = web3.eth.coinbase;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , abc: web3.eth.getBalance(coinbase).toNumber() });
+  res.render('index', { title: 'Express' , abc: web3.fromWei(web3.eth.getBalance(coinbase).toString(),'ether') });
 });
 
 
