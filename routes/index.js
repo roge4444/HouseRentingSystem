127dc2efe66d233db92e2a3f8a36d24fd4e0a429
Roger <ipadaxe@gmail.com>
2016-12-05 10:18:29 +0800

    Branch the web3 version 1.2

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var router = express.Router();
 //var io = require('socket.io').listen(server);
 
 
-var Web3 = require("web3");
+/*var Web3 = require("web3");
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var coinbase = web3.eth.coinbase;
+var coinbase = web3.eth.coinbase;*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , abc: web3.fromWei(web3.eth.getBalance(coinbase).toString(),'ether') });
+  res.render('index', { title: 'Express' });
 });
 
 
